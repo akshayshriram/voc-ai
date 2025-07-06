26b84b75e8a405fa1554c80704fce6f3277ffc5e
Akshay <akshayshriram999@gmail.com>
2025-07-06 22:16:52 +0530

    Set Vapi Workflow and Fetch interviews on home page

--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -1,85 +1,149 @@
-'use server'
+"use server";
 
-import { auth, db } from "@/firebase/admin"
-import { error } from "console"
+import { auth, db } from "@/firebase/admin";
+import { error, log } from "console";
 import { cookies } from "next/headers";
 
-const ONE_WEEK = 60*60*24*7
+const ONE_WEEK = 60 * 60 * 24 * 7;
 
-export async function signUp(params:SignUpParams) {
-    const {uid, name, email} = params
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
 
-    try{
-        const userRecord = await db.collection('users').doc(uid).get();
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
 
-        if(userRecord.exists){
-            return {
-                success: false,
-                message: 'User already exists. Please sign in instead.'
-            }
-        }
-        await db.collection('users').doc(uid).set({
-            name,
-            email
-        })
-
-        return{
-            success: true,
-            message: 'Account created successfully. Please sign in'
-        }
-
-    }catch(err: any){
-        console.error('Error creating a user:', err)
-        if(error.code === 'auth/email-already-exists'){
-            return{
-                success: false,
-                message: 'This email is already in use'
-            }
-        }
-
-        return {
-            success: false,
-            message: "Failed to create an account"
-        }
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: "User already exists. Please sign in instead.",
+      };
     }
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in",
+    };
+  } catch (err: any) {
+    console.error("Error creating a user:", err);
+    if (error.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "This email is already in use",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Failed to create an account",
+    };
+  }
 }
 
-export async function setSessionCookie(idToken:string){
-    const cookieStore = await cookies();
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
 
-    const sessionCookie = await auth.createSessionCookie(idToken,{
-        expiresIn: ONE_WEEK * 1000,
-    })
-    cookieStore.set('session', sessionCookie, {
-        maxAge: ONE_WEEK * 1000,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        path: '/',
-        sameSite: 'lax'
-    })
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000,
+  });
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK * 1000,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
 }
 
-export async function signIn(params: SignInParams){
-    const {email, idToken} = params
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
 
-    try{
-        const userRecord = await auth.getUserByEmail(email);
+  try {
+    const userRecord = await auth.getUserByEmail(email);
 
-        if(!userRecord){
-            return {
-                success: false,
-                message:'User does not exists. Create an account instead.'
-            }
-        }
-
-        await setSessionCookie(idToken)
-        
-    }catch(err){
-        console.error(err)
-        
-        return {
-            success: false,
-            message: 'Failed to Login'
-        }
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User does not exists. Create an account instead.",
+      };
     }
+
+    await setSessionCookie(idToken);
+  } catch (err) {
+    console.error(err);
+
+    return {
+      success: false,
+      message: "Failed to Login",
+    };
+  }
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+  try {
+    const decodeClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    // Get user info from db
+    const userRecord = await db.collection("users").doc(decodeClaims.uid).get();
+
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.error("ERROR:", error);
+  }
+}
+
+export async function isAuthenticate() {
+  const user = await getCurrentUser();
+  return !!user;
+}
+
+export async function getInterviewsByUserId(
+  userId: string
+): Promise<Interview[] | null> {
+  const interviews = await db
+    .collection("interviews")
+    .where("userId", "==", userId)
+    .orderBy("createdAt", "desc")
+    .get();
+
+  console.log("getInterviewsByUserId:", interviews);
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
+export async function getLatestInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 20 } = params;
+
+  if (!userId) {
+    console.error("getLatestInterviews: userId is undefined or null");
+    return null;
+  }
+  const interviews = await db
+    .collection("interviews")
+    .orderBy("createdAt", "desc")
+    .where("finalized", "==", true)
+    .where("userId", "!=", userId!)
+    .limit(limit)
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
 }
